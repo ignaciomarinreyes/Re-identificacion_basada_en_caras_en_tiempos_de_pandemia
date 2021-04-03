537b642b2350e9828a2ecf0049a7545a045e1b56
ignaciomarinreyes <ignaciomarinreyes@gmail.com>
2021-04-03 22:14:34 +0100

    v_1.02

--- FiabilityDetectionAlgorithm/test.py
+++ FiabilityDetectionAlgorithm/test.py
@@ -1,5 +1,6 @@
 import glob
 import numpy as np
+import sklearn.metrics as mt
 
 path="/Users/ignacio/TFG/TFG/data/LPATrail20-Salida_faces_prueba/"
 
@@ -9,33 +10,38 @@ def isInBox(tx, ty, bxi, bxe, byi, bye):
 def iswearingMask(maskTagged, maskResult):
     return maskTagged == maskResult
 
-totalPeople = 0
-totalSuccess = 0
+vPred = []
+vTrue = []
+found = False
 for pathTxtResult, pathTxtTagged in zip(sorted(glob.glob(path + "*result.txt")), sorted(glob.glob(path + "*tagged.txt"))):
     fileTagged = open(pathTxtTagged)
     lineResultV = np.loadtxt(pathTxtResult).astype(int)
-    numberPeople = 0
-    numberSuccess = 0
     for lineTagged in fileTagged:
         valuesLineTagged = lineTagged.split(" ")
-        numberPeople += 1
+        vTrue.append(int(valuesLineTagged[2]))
         for valuesLineResult in lineResultV:
+            print("---------------------")
             x1 = valuesLineResult[0] + valuesLineResult[2]
             y1 = valuesLineResult[1] + valuesLineResult[3]
             print("Labed coordenada: x: " + str(valuesLineTagged[0]) + " y: " + str(valuesLineTagged[1]) + " Mask: " + str(valuesLineTagged[2]))
             print("Result coordenada: x0: " + str(valuesLineResult[0]) + " x1: " + str(x1) + " y0: " + str(valuesLineResult[1]) + " y1: " + str(y1) + " Mask: " + str(valuesLineResult[4]))
             if isInBox(int(valuesLineTagged[0]), int(valuesLineTagged[1]), valuesLineResult[0], x1, valuesLineResult[1], y1) and iswearingMask(int(valuesLineTagged[2]), valuesLineResult[4]):
-                numberSuccess+=1
+                found = True
                 print("Success")
-            print("Number success: " + str(numberSuccess))
-            print("Number people: " + str(numberPeople))
-            print("---------------------")
+                break
+        if found:
+            vPred.append(1)
+        else:
+            vPred.append(0)
+        found = False
         print("-----Change person ------")
-    totalPeople += numberPeople
-    totalSuccess += numberSuccess
     print("============Change file==================")
-print("Number total success: " + str(totalSuccess))
-print("Number total people: " + str(totalPeople))
-print("Hit rate: " + str((totalSuccess/totalPeople)*100))
+print("vPred:")
+print(vPred)
+print("vTrue:")
+print(vTrue)
+print("======Metrics=====")
+print(mt.accuracy_score(vTrue, vPred))
+
 
 
