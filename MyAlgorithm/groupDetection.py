5e1db5b5cac1a219c969c4ce4929470b92ef0f41
ignaciomarinreyes <ignaciomarinreyes@gmail.com>
2021-05-09 01:37:23 +0100

    agrupar id únicos 2

--- MyAlgorithm/groupDetection.py
+++ MyAlgorithm/groupDetection.py
@@ -33,29 +33,29 @@ for pathTxtFace in sorted(glob.glob(path + "*faces.txt")):
                 if newId in dGroup:
                     vContent = dGroup[newId].copy()
                     if dIdsFace[id][4] == '1':
-                        vContent[4] += 1
+                        vContent[8] += 1
                     if dIdsFace[id][4] == '0':
-                        vContent[5] += 1
+                        vContent[9] += 1
                     if dIdsFace[id][4] == 'ND':
-                        vContent[6] += 1
-                    vContent[3] = timeFile
+                        vContent[10] += 1
+                    vContent[7] = timeFile
                     dGroup[newId] = vContent
                 else:
-                    dGroup[newId] = [newId, 0, timeFile, None, 0, 0, 0]
+                    dGroup[newId] = [newId, dIdsFace[id][0], dIdsFace[id][1], dIdsFace[id][2], dIdsFace[id][3],0, timeFile, timeFile, 1 if dIdsFace[id][4] == '1' else 0, 1 if dIdsFace[id][4] == '0' else 0, 1 if dIdsFace[id][4] == 'ND' else 0]
             else:
                 if id in dGroup:
                     vContent = dGroup[id].copy()
                     if dIdsFace[id][4] == '1':
-                        vContent[4] += 1
+                        vContent[8] += 1
                     if dIdsFace[id][4] == '0':
-                        vContent[5] += 1
+                        vContent[9] += 1
                     if dIdsFace[id][4] == 'ND':
-                        vContent[6] += 1
-                    vContent[3] = timeFile
+                        vContent[10] += 1
+                    vContent[7] = timeFile
                     dGroup[id] = vContent
                 else:
-                    dGroup[id] = [id, 0, timeFile, None, 0, 0, 0]
+                    dGroup[id] = [id, dIdsFace[id][0], dIdsFace[id][1], dIdsFace[id][2], dIdsFace[id][3], 0, timeFile, timeFile, 1 if dIdsFace[id][4] == '1' else 0, 1 if dIdsFace[id][4] == '0' else 0, 1 if dIdsFace[id][4] == 'ND' else 0]
 print(dGroup)
 fileGroupDetection = open(path + "groupDetection.txt", "w")
 for id in dGroup:
-    fileGroupDetection.write(str(dGroup[id][0]) + " " + str(dGroup[id][1]) + " " + str(dGroup[id][2]) + " " + str(dGroup[id][3]) + " " + str(dGroup[id][4]) + " " + str(dGroup[id][5]) + " " + str(dGroup[id][6]) + "\n")
+    fileGroupDetection.write(str(dGroup[id][0]) + " " + str(dGroup[id][1]) + " " + str(dGroup[id][2]) + " " + str(dGroup[id][3]) + " " + str(dGroup[id][4]) + " " + str(dGroup[id][5]) + " " + str(dGroup[id][6]) + " " + str(dGroup[id][7])+ " " + str(dGroup[id][8])+ " " + str(dGroup[id][9])+ " " + str(dGroup[id][10]) + " \n")
