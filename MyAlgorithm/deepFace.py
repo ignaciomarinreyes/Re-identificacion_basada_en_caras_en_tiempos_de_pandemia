939fd407fe583fe8850573ad497dcdd12cb4ec01
ignaciomarinreyes <ignaciomarinreyes@gmail.com>
2021-05-17 00:20:58 +0100

    AlignedReID

--- MyAlgorithm/deepFace.py
+++ MyAlgorithm/deepFace.py
@@ -4,7 +4,7 @@ import glob
 path="/content/gdrive/My Drive/TFG/data/Bodies/"
 #path="/Users/ignacio/TFG/TFG/data/Faces/"
 
-for pathPng1 in sorted(glob.glob(path + "*.jpg")):
-    for pathPng2 in sorted(glob.glob(path + "*.jpg")):
-        result  = DeepFace.verify(pathPng1, pathPng2)
-        print("JPG_1 " + pathPng1  + " JPG_2 " +  pathPng2 +  "===> " + str(result["verified"]))
+for pathJpg1 in sorted(glob.glob(path + "*.jpg")):
+    for pathJpg2 in sorted(glob.glob(path + "*.jpg")):
+        result  = DeepFace.verify(pathJpg1, pathJpg2, model_name ="Facenet", enforce_detection = False)
+        print("JPG_1 " + pathJpg1 + " JPG_2 " + pathJpg2 + "===> " + str(result["verified"]))
