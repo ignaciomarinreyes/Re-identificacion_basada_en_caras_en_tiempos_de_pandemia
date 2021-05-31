1f58906453a7d6045922fa531eb33dfa475e4ae0
ignaciomarinreyes <ignaciomarinreyes@gmail.com>
2021-06-01 00:51:47 +0100

    DeepFace con vector optimizado

--- MyAlgorithm/deepFace.py
+++ MyAlgorithm/deepFace.py
@@ -23,111 +23,18 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
-def verifyVectorFeature(vImage1, vImage2, img1_path, img2_path, model_name = 'VGG-Face', distance_metric = 'cosine', model = None, enforce_detection = True, detector_backend = 'mtcnn', align = True):
-	tic = time.time()
-	img_list, bulkProcess = functions.initialize_input(img1_path, img2_path)
-	functions.initialize_detector(detector_backend = detector_backend)
-	resp_objects = []
-	#--------------------------------
-	if model_name == 'Ensemble':
-		model_names = ["VGG-Face", "Facenet", "OpenFace", "DeepFace"]
-		metrics = ["cosine", "euclidean", "euclidean_l2"]
-	else:
-		model_names = []; metrics = []
-		model_names.append(model_name)
-		metrics.append(distance_metric)
-	#--------------------------------
-	if model == None:
-		if model_name == 'Ensemble':
-			models = Boosting.loadModel()
+def distanceVectorFeature(vImage1, vImage2, distance_metric = 'cosine'):
+		#metrics = ["cosine", "euclidean", "euclidean_l2"]
+		if distance_metric == 'cosine':
+			distance = dst.findCosineDistance(vImage1, vImage2)
+		elif distance_metric == 'euclidean':
+			distance = dst.findEuclideanDistance(vImage1, vImage2)
+		elif distance_metric == 'euclidean_l2':
+			distance = dst.findEuclideanDistance(dst.l2_normalize(vImage1), dst.l2_normalize(vImage2))
 		else:
-			model = DeepFace.build_model(model_name)
-			models = {}
-			models[model_name] = model
-	else:
-		if model_name == 'Ensemble':
-			Boosting.validate_model(model)
-			models = model.copy()
-		else:
-			models = {}
-			models[model_name] = model
-	#------------------------------
-	#calling deepface in a for loop causes lots of progress bars. this prevents it.
-	disable_option = False if len(img_list) > 1 else True
-	pbar = tqdm(range(0,len(img_list)), desc='Verification', disable = disable_option)
-	for index in pbar:
-		instance = img_list[index]
-		if type(instance) == list and len(instance) >= 2:
-			img1_path = instance[0]; img2_path = instance[1]
-			ensemble_features = []
-			for i in  model_names:
-				custom_model = models[i]
-				#img_path, model_name = 'VGG-Face', model = None, enforce_detection = True, detector_backend = 'mtcnn'
-				img1_representation = vImage1
-				img2_representation = vImage2
-				#----------------------
-				#find distances between embeddings
-				for j in metrics:
-					if j == 'cosine':
-						distance = dst.findCosineDistance(img1_representation, img2_representation)
-					elif j == 'euclidean':
-						distance = dst.findEuclideanDistance(img1_representation, img2_representation)
-					elif j == 'euclidean_l2':
-						distance = dst.findEuclideanDistance(dst.l2_normalize(img1_representation), dst.l2_normalize(img2_representation))
-					else:
-						raise ValueError("Invalid distance_metric passed - ", distance_metric)
-					distance = np.float64(distance) #causes trobule for euclideans in api calls if this is not set (issue #175)
-					#----------------------
-					#decision
-					if model_name != 'Ensemble':
-						threshold = dst.findThreshold(i, j)
-						if distance <= threshold:
-							identified = True
-						else:
-							identified = False
-						resp_obj = {
-							"verified": identified
-							, "distance": distance
-							, "max_threshold_to_verify": threshold
-							, "model": model_name
-							, "similarity_metric": distance_metric
-						}
-						if bulkProcess == True:
-							resp_objects.append(resp_obj)
-						else:
-							return resp_obj
-					else: #Ensemble
-						#this returns same with OpenFace - euclidean_l2
-						if i == 'OpenFace' and j == 'euclidean':
-							continue
-						else:
-							ensemble_features.append(distance)
-			#----------------------
-			if model_name == 'Ensemble':
-				boosted_tree = Boosting.build_gbm()
-				prediction = boosted_tree.predict(np.expand_dims(np.array(ensemble_features), axis=0))[0]
-				verified = np.argmax(prediction) == 1
-				score = prediction[np.argmax(prediction)]
-				resp_obj = {
-					"verified": verified
-					, "score": score
-					, "distance": ensemble_features
-					, "model": ["VGG-Face", "Facenet", "OpenFace", "DeepFace"]
-					, "similarity_metric": ["cosine", "euclidean", "euclidean_l2"]
-				}
-				if bulkProcess == True:
-					resp_objects.append(resp_obj)
-				else:
-					return resp_obj
-		else:
-			raise ValueError("Invalid arguments passed to verify function: ", instance)
-	toc = time.time()
-	if bulkProcess == True:
-		resp_obj = {}
-		for i in range(0, len(resp_objects)):
-			resp_item = resp_objects[i]
-			resp_obj["pair_%d" % (i+1)] = resp_item
-		return resp_obj
+			raise ValueError("Invalid distance_metric passed - ", distance_metric)
+		distance = np.float64(distance)
+		return distance
 
 
 def deepFaceInterVideo():
@@ -154,8 +61,8 @@ def deepFaceInterVideo():
                         for line2 in fileOutput2:
                             vectorFeature2.append(float(line2))
                         print(dirpath1 + "/" + file1 + " ===> " + dirpath2 + "/" + file2 + " ===> " )
-                        result = verifyVectorFeature(vectorFeature1, vectorFeature2,dirpath1 + "/" + file1[0:-18] + "body.png",dirpath2 + "/" + file2[0:-18] + "body.png", model_name="Facenet", enforce_detection=False)
-                        fileOutput.write(place1 + " " + id1 + " " + place2 + " " + id2 + " " +  str(result["distance"]) + " \n")
+                        distance = distanceVectorFeature(vectorFeature1, vectorFeature2)
+                        fileOutput.write(place1 + " " + timeFile1 + " " + id1 + " " + place2 + " " + timeFile2 + " " + id2 + " " +  str(distance) + " \n")
 
 
 def deepFaceIntraVideo():
@@ -165,6 +72,7 @@ def deepFaceIntraVideo():
             print("png_1 " + pathPng1 + " png_2 " + pathPng2 + "===> " + str(result["distance"]))
 
 
+# model_names = ["VGG-Face", "Facenet", "OpenFace", "DeepFace"]
 def deepFaceVector():
     for dirpath, dirnames, filenames in os.walk(params.path):
         filenames = [f for f in filenames if not f[0] == '.']
@@ -179,7 +87,6 @@ def deepFaceVector():
             fileOutput = open(dirpath + "/" + timeFile + "_" + id + "_" + "deepFaceVector.txt", "w")
             for value in result:
                 fileOutput.write(str(value) + "\n")
-
 
 
 if __name__ == '__main__':
