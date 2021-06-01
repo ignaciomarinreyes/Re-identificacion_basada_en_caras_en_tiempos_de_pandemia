4d9afc476e635324c763a99155e8dc04f6457ca0
ignaciomarinreyes <ignaciomarinreyes@gmail.com>
2021-06-01 14:58:37 +0100

    DeepFaceDistance varios modelos y distancias

--- MyAlgorithm/deepFace.py
+++ MyAlgorithm/deepFace.py
@@ -13,8 +13,8 @@ params = None
 def parse_arguments():
     parser = argparse.ArgumentParser(description='DeepFace script')
     parser.add_argument('-path', '-p' ,default="/content/gdrive/My Drive/TFG/data/Reidentification", help='Path to apply DeepFace')
-    parser.add_argument('-inter', '-ie', action='store_true')
-    parser.add_argument('-intra', '-ir', action='store_true')
+    parser.add_argument('-distInter', '-die', action='store_true')
+    parser.add_argument('-distIntra', '-dia', action='store_true')
     parser.add_argument('-vector', '-v', action='store_true')
     args = parser.parse_args()
     return args
@@ -30,11 +30,12 @@ def distanceVectorFeature(vImage1, vImage2):
     return distanceCosine, distanceEuclidean, distanceEuclidean_l2
 
 
-def deepFaceInterVideo():
-    model_names = ["VGG-Face", "Facenet", "OpenFace", "DeepFace"]
+def distanceDeepFaceInterVideo():
+    model_names = ["VGG-Face", "Facenet", "OpenFace", "DeepFace", "DeepID", "Dlib", "ArcFace"]
     for model in model_names:
+        print("============= " + model + " ===================")
         for dirpath1, dirnames1, filenames1 in os.walk(params.path):
-            filenames1 = [f for f in filenames1 if not f[0] == '.' and f[-18:] == 'deepFaceVector.txt' and f.split("_")[2] == model]
+            filenames1 = [f for f in filenames1 if not f[0] == '.' and f[-18:] == 'deepFaceVector.txt' and f.split("_")[5] == model]
             for file1 in sorted(filenames1):
                 pathWithOutBaseName1, id1 = os.path.split(dirpath1)
                 x, place1 = os.path.split(pathWithOutBaseName1)
@@ -43,16 +44,16 @@ def deepFaceInterVideo():
                 fileOutput1 = open(dirpath1 + "/" + timeFile1 + "_" + id1 + "_" + model +"_deepFaceVector.txt")
                 for line1 in fileOutput1:
                     vectorFeature1.append(float(line1))
-                fileOutput = open(dirpath1 + "/" + timeFile1 + "_" + id1 + "_" + model + "_deepFaceInterResult.txt", "w")
+                fileOutput = open(dirpath1 + "/" + timeFile1 + "_" + id1 + "_" + model + "_deepFaceInterDistance.txt", "w")
                 for dirpath2, dirnames2, filenames2 in os.walk(params.path):
-                    filenames2 = [f for f in filenames2 if not f[0] == '.' and f[-18:] == 'deepFaceVector.txt' and f.split("_")[2] == model]
+                    filenames2 = [f for f in filenames2 if not f[0] == '.' and f[-18:] == 'deepFaceVector.txt' and f.split("_")[5] == model]
                     for file2 in sorted(filenames2):
                         pathWithOutBaseName2, id2 = os.path.split(dirpath2)
                         y, place2 = os.path.split(pathWithOutBaseName2)
                         timeFile2 = file2[0: 12]
                         if place1 != place2:
                             vectorFeature2 = []
-                            fileOutput2 = open(dirpath2 + "/" + timeFile2 + "_" + id2 + "_" + model + "_deepFaceInterResult.txt")
+                            fileOutput2 = open(dirpath2 + "/" + timeFile2 + "_" + id2 + "_" + model + "_deepFaceVector.txt")
                             for line2 in fileOutput2:
                                 vectorFeature2.append(float(line2))
                             print(dirpath1 + "/" + file1 + " ===> " + dirpath2 + "/" + file2 + " ===> " )
@@ -60,7 +61,7 @@ def deepFaceInterVideo():
                             fileOutput.write(place1 + " " + timeFile1 + " " + id1 + " " + place2 + " " + timeFile2 + " " + id2 + " " +  str(distanceCosine) + " " +  str(distanceEuclidean)  + " " +  str(distanceEuclidean_l2)  + " \n")
 
 
-def deepFaceIntraVideo():
+def distanceDeepFaceIntraVideo():
     for pathPng1 in sorted(glob.glob(params.path + "*.png")):
         for pathPng2 in sorted(glob.glob(params.path + "*.png")):
             result = DeepFace.verify(pathPng1, pathPng2, model_name="Facenet", enforce_detection=False)
@@ -89,7 +90,7 @@ if __name__ == '__main__':
     params = parse_arguments()
     if params.vector:
         deepFaceVector()
-    if params.inter:
-        deepFaceInterVideo()
-    if params.intra:
-        deepFaceIntraVideo()
+    if params.distInter:
+        distanceDeepFaceInterVideo()
+    if params.distIntra:
+        distanceDeepFaceIntraVideo()
